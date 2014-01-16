7d4a0882ebaca4d2f8e1b2631b5a7c6f59b33208
Isaac Murchie <isaac@saucelabs.com>
2014-01-16 12:14:15 -0800

    Actually install selenium-webdriver gem

--- install_tutorial.sh
+++ install_tutorial.sh
@@ -7,7 +7,8 @@ if ! gem list rspec -i > /dev/null; then
 fi
 
 if ! gem list selenium-webdriver -i > /dev/null; then
-  echo "Install Selenium WebDriver gem"
+  echo "Installing Selenium WebDriver gem"
+  gem install selenium-webdriver
 fi
 
 echo "Downloading SauceTest.rb to the current directory"
